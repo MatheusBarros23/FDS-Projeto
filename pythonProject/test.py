99534716d6a26711b07d01fd8c479576aa91a0b9
MatheusBarros23 <mprb@cesar.school>
2022-05-25 09:37:44 -0300

    #

--- pythonProject/test.py
+++ pythonProject/test.py
@@ -5,6 +5,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 
 navegador = webdriver.Chrome()
-navegador.get("http://127.0.0.1:5000/")
+navegador.get("process.env.PORT")
 navegador.find.element_by_xpath('/html/body/header/div[2]/nav/ul/li[1]/a').click()
 time.sleep(5)
