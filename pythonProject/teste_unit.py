64cd5667b65bd6876561dcf1e34ff01b997b50ac
Eduardo <leam@cesar.school>
2022-06-05 23:24:29 -0300

    Update Tests with Unittes
    Add more 2 test`s

--- pythonProject/teste_unit.py
+++ pythonProject/teste_unit.py
@@ -8,6 +8,8 @@ class TestSalvarDao(unittest.TestCase):
         self.jogo1 = Jogo('CSGO', 'FPS', 'PC')
         self.jogo2 = Jogo('GTA', 'ARCADE', 'PC')
         self.user1 = Usuario('ADE', 'Dzr', 'sportcampeao87')
+        self.user2 = Usuario('3705', 'Kryno', 'lolmaster')
+        
 
     def test_input_jogo(self):
         
@@ -15,12 +17,23 @@ class TestSalvarDao(unittest.TestCase):
         self.assertEqual(self.jogo1.categoria, 'FPS')
         self.assertEqual(self.jogo1.console, 'PC')
 
+        self.assertEqual(self.jogo2.nome, 'GTA')
+        self.assertEqual(self.jogo2.categoria, 'ARCADE')
+        self.assertEqual(self.jogo2.console, 'PC')
+
     def test_input_usuario(self):
 
         self.assertEqual(self.user1.id, 'ADE')
         self.assertEqual(self.user1.nome, 'Dzr')
         self.assertEqual(self.user1.senha, 'sportcampeao87')
 
+        self.assertEqual(self.user2.id, '3705')
+        self.assertEqual(self.user2.nome, 'Kryno')
+        self.assertEqual(self.user2.senha, 'lolmaster')
+        
+
+
 if __name__ == '__main__':
+
     print("---Unit Tests---")
     unittest.main()
